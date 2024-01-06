adc10fdd5411bfd7c349597cbae10b00940d2027
Nobel Suhendra <71556513+NobelSuhendra@users.noreply.github.com>
2024-01-06 22:35:32 +0700

    Added quantity select to favourites page, will need to fix favourites page ui as well as add more info to quantity select i.e stock rem

--- components/Favourite.js
+++ components/Favourite.js
@@ -1,6 +1,6 @@
 import { NavigationContainer, useIsFocused } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect, useRef } from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
   KeyboardAvoidingView,
@@ -17,6 +17,7 @@ import {
   FlatList,
   Dimensions,
   Keyboard,
+  Modal,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import * as Progress from "react-native-progress";
@@ -34,11 +35,15 @@ import {
 import { NetworkContext } from "../exports";
 
 const SCWIDTH = Dimensions.get("window").width;
+const SCHEIGHT = Dimensions.get("window").height;
 
 export default function Favourites() {
   const [data, setData] = useState({});
   const [changed, setChanged] = useState(0);
   const [val, setVal] = useState("");
+  const [purchData, setPurchData] = useState({});
+  const [vis, setVis] = useState(false);
+  const [pur, setPur] = useState(1);
   const value = useContext(NetworkContext);
   const user = value.params.user;
   const isFocused = useIsFocused();
@@ -68,6 +73,160 @@ export default function Favourites() {
   }, [changed, isFocused, val]);
   return (
     <View style={{ height: "100%", width: "100%" }}>
+      <Modal transparent={true} visible={vis} animationType="fade">
+        <Pressable
+          style={{
+            height: SCHEIGHT - 370,
+            width: "100%",
+            justifyContent: "flex-end",
+            backgroundColor: `rgba(0, 0, 0, 0.6)`,
+          }}
+          onPress={() => {
+            setVis(false);
+            setPur(1);
+          }}
+        />
+        <View
+          style={{
+            backgroundColor: "white",
+            width: "100%",
+            height: 370,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              fontWeight: 800,
+              fontSize: 20,
+              textAlign: "center",
+              width: "90%",
+            }}
+            numberOfLines={1}
+          >
+            {purchData.name}
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              marginBottom: 20,
+              justifyContent: "center",
+              alignItems: "center",
+              width: "90%",
+              marginTop: 10,
+            }}
+          >
+            <Ionicons name="time" size={20} style={{ color: "#BF41B7" }} />
+            <Text
+              style={{
+                marginLeft: 3,
+                textAlign: "center",
+              }}
+              numberOfLines={1}
+            >
+              {purchData.collectionStart} - {purchData.collectionEnd}
+            </Text>
+          </View>
+          <View
+            style={{
+              alignItems: "center",
+              width: "90%",
+              borderTopWidth: 0.2,
+              borderBottomWidth: 0.2,
+              paddingTop: 15,
+              paddingBottom: 5,
+            }}
+          >
+            <Text>Select quantity...</Text>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <Pressable
+                disabled={pur == 1}
+                style={{
+                  width: 32,
+                  height: 32,
+                  borderWidth: 1,
+                  borderColor: pur == 1 ? "#e9e9e9" : "#BF41B7",
+                  backgroundColor: pur == 1 ? "#e9e9e9" : "white",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 100,
+                }}
+                onPress={() => {
+                  setPur(pur - 1);
+                }}
+              >
+                <Ionicons
+                  name="remove-outline"
+                  size={15}
+                  style={{ color: pur == 1 ? "white" : "#BF41B7" }}
+                />
+              </Pressable>
+              <Text
+                style={{
+                  marginLeft: 15,
+                  marginRight: 15,
+                  fontSize: 50,
+                  fontWeight: 900,
+                }}
+              >
+                {pur}
+              </Text>
+              <Pressable
+                disabled={pur == purchData.stock}
+                style={{
+                  width: 32,
+                  height: 32,
+                  borderWidth: 1,
+                  borderColor: pur == purchData.stock ? "#e9e9e9" : "#BF41B7",
+                  backgroundColor: pur == purchData.stock ? "#e9e9e9" : "white",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 100,
+                }}
+                onPress={() => {
+                  setPur(pur + 1);
+                }}
+              >
+                <Ionicons
+                  name="add-outline"
+                  size={15}
+                  style={{
+                    color: pur == purchData.stock ? "white" : "#BF41B7",
+                  }}
+                />
+              </Pressable>
+            </View>
+          </View>
+          <View style={{ width: "90%", flexDirection: "row", marginTop: 15 }}>
+            <View style={{ flex: 1 }}>
+              <Text>Total</Text>
+            </View>
+            <View style={{ flex: 1, alignItems: "flex-end" }}>
+              <Text>${(purchData.price * pur).toFixed(2)}</Text>
+            </View>
+          </View>
+          <Pressable
+            style={{
+              width: "90%",
+              backgroundColor: "#BF41B7",
+              height: 40,
+              marginLeft: "2%",
+              borderRadius: "10%",
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: 50,
+              marginBottom: 10,
+            }}
+          >
+            <Text style={{ color: "white", fontSize: 14 }}>Confirm</Text>
+          </Pressable>
+        </View>
+      </Modal>
       <View
         style={{
           width: "90%",
@@ -214,6 +373,10 @@ export default function Favourites() {
                         borderRadius: 10,
                         borderWidth: 2,
                         borderColor: "#BF41B7",
+                      }}
+                      onPress={() => {
+                        setPurchData(item);
+                        setVis(true);
                       }}
                     >
                       <Text
