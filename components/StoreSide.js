9f2072a3f5c50e857cf42434bb7700dd5ff13b34
Nobel Suhendra <71556513+NobelSuhendra@users.noreply.github.com>
2024-02-03 12:20:49 +0700

    Made basic store side view

--- components/StoreSide.js
+++ components/StoreSide.js
@@ -36,10 +36,38 @@ import {
 import { NetworkContext } from "../exports";
 
 export default function StoreSide() {
+  const value = useContext(NetworkContext);
+  const user = value.params.user;
   const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
+  const [loc, setLoc] = useState("");
+  const [otime, setOtime] = useState("");
+  const [etime, setEtime] = useState("");
+  const [stock, setStock] = useState(0);
+  const [edit, setEdit] = useState(false);
+  const [pressed, setPressed] = useState(false);
+  useEffect(() => {
+    async function Temp() {
+      const docRef = doc(db, "stores", user);
+      const docSnap = await getDoc(docRef);
+      setName(docSnap.data().name);
+      setDesc(docSnap.data().desc);
+      setLoc(docSnap.data().loc);
+      setOtime(docSnap.data().collectionStart);
+      setEtime(docSnap.data().collectionEnd);
+      setStock(docSnap.data().stock);
+    }
+    Temp();
+  }, []);
+
   const SCWIDTH = Dimensions.get("window").width;
   return (
-    <View style={{ width: "100%", height: "100%" }}>
+    <KeyboardAvoidingView
+      style={{ flex: 1, flexDirection: "column", justifyContent: "center" }}
+      behavior="padding"
+      enabled
+      keyboardVerticalOffset={50}
+    >
       <ScrollView>
         <Text
           style={{
@@ -51,7 +79,6 @@ export default function StoreSide() {
         >
           My store
         </Text>
-        <View></View>
         <View
           style={{
             height: 75,
@@ -73,29 +100,284 @@ export default function StoreSide() {
               marginTop: 5,
             }}
           >
-            <Ionicons
-              name={"lock-closed-outline"}
-              size={18}
-              style={{ color: "#400235", marginRight: 10, marginTop: 3 }}
-            />
             <TextInput
               placeholder={"Type here..."}
               value={name}
               onChangeText={(text) => setName(text)}
-              secureTextEntry={true}
               autoCapitalize="none"
               autoComplete="off"
-              editable
+              editable={edit}
               style={{
                 color: "#400235",
                 height: 25,
                 fontSize: 14,
-                width: SCWIDTH * 0.9 - 30 - 30,
+                width: SCWIDTH * 0.9 - 30,
               }}
             />
           </View>
         </View>
+        <View
+          style={{
+            height: 250,
+            width: "90%",
+            backgroundColor: "#F5F5F5",
+            borderRadius: 10,
+            padding: 15,
+            marginTop: 10,
+            marginLeft: "5%",
+          }}
+        >
+          <Text style={{ fontSize: 12, fontWeight: 700, color: "#BF41B7" }}>
+            DESCRIPTION
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              marginTop: 5,
+            }}
+          >
+            <TextInput
+              placeholder={"Type here..."}
+              textAlign="top"
+              value={desc}
+              onChangeText={(text) => setDesc(text)}
+              autoComplete="off"
+              multiline={true}
+              editable={edit}
+              style={{
+                color: "#400235",
+                height: 200,
+                fontSize: 14,
+                width: SCWIDTH * 0.9 - 30,
+                textAlignVertical: "top",
+              }}
+            />
+          </View>
+        </View>
+        <View
+          style={{
+            height: 75,
+            width: "90%",
+            backgroundColor: "#F5F5F5",
+            borderRadius: 10,
+            padding: 15,
+            justifyContent: "center",
+            marginTop: 15,
+            marginLeft: "5%",
+          }}
+        >
+          <Text style={{ fontSize: 12, fontWeight: 700, color: "#BF41B7" }}>
+            LOCATION
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              marginTop: 5,
+            }}
+          >
+            <TextInput
+              placeholder={"Type here..."}
+              value={loc}
+              onChangeText={(text) => setLoc(text)}
+              autoCapitalize="none"
+              autoComplete="off"
+              editable={edit}
+              style={{
+                color: "#400235",
+                height: 25,
+                fontSize: 14,
+                width: SCWIDTH * 0.9 - 30,
+              }}
+            />
+          </View>
+        </View>
+        <View
+          style={{
+            height: 75,
+            width: "90%",
+            backgroundColor: "#F5F5F5",
+            borderRadius: 10,
+            padding: 15,
+            justifyContent: "center",
+            marginTop: 15,
+            marginLeft: "5%",
+          }}
+        >
+          <Text style={{ fontSize: 12, fontWeight: 700, color: "#BF41B7" }}>
+            COLLECTION START
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              marginTop: 5,
+            }}
+          >
+            <TextInput
+              placeholder={"Type here..."}
+              value={otime}
+              onChangeText={(text) => setOtime(text)}
+              autoCapitalize="none"
+              autoComplete="off"
+              editable={edit}
+              style={{
+                color: "#400235",
+                height: 25,
+                fontSize: 14,
+                width: SCWIDTH * 0.9 - 30,
+              }}
+            />
+          </View>
+        </View>
+        <View
+          style={{
+            height: 75,
+            width: "90%",
+            backgroundColor: "#F5F5F5",
+            borderRadius: 10,
+            padding: 15,
+            justifyContent: "center",
+            marginTop: 15,
+            marginLeft: "5%",
+          }}
+        >
+          <Text style={{ fontSize: 12, fontWeight: 700, color: "#BF41B7" }}>
+            COLLECTION END
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              marginTop: 5,
+            }}
+          >
+            <TextInput
+              placeholder={"Type here..."}
+              value={etime}
+              onChangeText={(text) => setEtime(text)}
+              autoCapitalize="none"
+              autoComplete="off"
+              editable={edit}
+              style={{
+                color: "#400235",
+                height: 25,
+                fontSize: 14,
+                width: SCWIDTH * 0.9 - 30,
+              }}
+            />
+          </View>
+        </View>
+        <View
+          style={{
+            height: 75,
+            width: "90%",
+            backgroundColor: "#F5F5F5",
+            borderRadius: 10,
+            padding: 15,
+            justifyContent: "center",
+            marginTop: 15,
+            marginLeft: "5%",
+          }}
+        >
+          <Text style={{ fontSize: 12, fontWeight: 700, color: "#BF41B7" }}>
+            STOCK
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              marginTop: 5,
+            }}
+          >
+            <TextInput
+              placeholder={"Type here..."}
+              value={stock}
+              keyboardType="numeric"
+              onChangeText={(text) => setStock(text)}
+              autoCapitalize="none"
+              autoComplete="off"
+              editable={edit}
+              style={{
+                color: "#400235",
+                height: 25,
+                fontSize: 14,
+                width: SCWIDTH * 0.9 - 30,
+              }}
+            />
+          </View>
+        </View>
+        {!edit ? (
+          <Pressable
+            style={{
+              width: "90%",
+              backgroundColor: "#BF41B7",
+              height: 50,
+              marginLeft: "5%",
+              borderRadius: 15,
+              marginTop: 20,
+              marginBottom: 20,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            onPress={() => {
+              setEdit(true);
+            }}
+          >
+            <Text style={{ color: "white" }}>Edit</Text>
+          </Pressable>
+        ) : (
+          <View
+            style={{
+              flexDirection: "row",
+            }}
+          >
+            <Pressable
+              style={{
+                width: "42.5%",
+                borderColor: "#BF41B7",
+                height: 50,
+                marginLeft: "5%",
+                borderRadius: 15,
+                marginTop: 20,
+                marginBottom: 20,
+                justifyContent: "center",
+                alignItems: "center",
+                borderWidth: 2,
+              }}
+              onPress={() => {
+                setEdit(false);
+              }}
+            >
+              <Text style={{ color: "#BF41B7" }}>Cancel</Text>
+            </Pressable>
+            <Pressable
+              style={{
+                width: "42.5%",
+                backgroundColor: "#BF41B7",
+                height: 50,
+                marginLeft: "5%",
+                borderRadius: 15,
+                marginTop: 20,
+                marginBottom: 20,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              onPress={async () => {
+                const docRef = doc(db, "stores", user);
+                await updateDoc(docRef, {
+                  name: name,
+                  desc: desc,
+                  loc: loc,
+                  collectionStart: otime,
+                  collectionEnd: etime,
+                  stock: stock,
+                });
+                setPressed(!pressed);
+                setEdit(false);
+              }}
+            >
+              <Text style={{ color: "white" }}>Save</Text>
+            </Pressable>
+          </View>
+        )}
       </ScrollView>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
